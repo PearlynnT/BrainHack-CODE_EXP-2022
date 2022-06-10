dc87983250e3c4cb3940af70eb7bb314fba8608d
Pearlynn <pearlynntoh@gmail.com>
2022-06-10 16:08:41 +0800

    Added TextInput & Button to login & register screens

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,45 @@
+// TODO: Add authentication
+
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 const LoginScreen = (props) => {
+  const [text, onChangeText] = React.useState(null);
+
+  const onLoginPress = () => {}; // TODO
+
   return (
     <View style={styles.container}>
-      <Text>Log In</Text>
+      <Text>Log In (Change this to a logo)</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        autoCapitalize="none"
+        autoFocus={true}
+        onChangeText={onChangeText}
+        //value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        autoCapitalize="none"
+        onChangeText={onChangeText}
+        //value={password}
+      />
+      <TouchableOpacity style={styles.button} onPress={() => onLoginPress()}>
+        <Text style={styles.buttonTitle}>Log in</Text>
+      </TouchableOpacity>
       <TouchableOpacity onPress={() => props.navigation.navigate("Register")}>
-        <Text>Don't have an account? Click here to register.</Text>
+        <Text style={styles.footerText}>
+          Don't have an account? Click here to register.
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -18,6 +51,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    width: 350,
+    height: 55,
+    margin: 10,
+    padding: 8,
+    borderRadius: 15,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  button: {
+    width: 350,
+    height: 55,
+    margin: 10,
+    padding: 8,
+    borderRadius: 15,
+    backgroundColor: "#000080",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  footerText: {
+    color: "#0000EE",
+    fontSize: 15,
   },
 });
 
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,13 +1,57 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+// TODO: Add authentication
 
-export default function RegisterScreen() {
+import React from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const RegisterScreen = (props) => {
+  const [text, onChangeText] = React.useState(null);
+
+  const onRegisterPress = () => {}; // TODO
+
   return (
     <View style={styles.container}>
-      <Text>Register</Text>
+      <Text>Register (Change this to a logo)</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        autoCapitalize="none"
+        autoFocus={true}
+        onChangeText={onChangeText}
+        //value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        autoCapitalize="none"
+        onChangeText={onChangeText}
+        //value={password}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        secureTextEntry={true}
+        autoCapitalize="none"
+        onChangeText={onChangeText}
+        //value={confirmPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={() => onRegisterPress()}>
+        <Text style={styles.buttonTitle}>Register</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => props.navigation.navigate("Log In")}>
+        <Text style={styles.footerText}>
+          Already have an account? Click here to log in.
+        </Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +60,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  input: {
+    width: 350,
+    height: 55,
+    margin: 10,
+    padding: 8,
+    borderRadius: 15,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  button: {
+    width: 350,
+    height: 55,
+    margin: 10,
+    padding: 8,
+    borderRadius: 15,
+    backgroundColor: "#000080",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  footerText: {
+    color: "#0000EE",
+    fontSize: 15,
+  },
 });
+
+export default RegisterScreen;
