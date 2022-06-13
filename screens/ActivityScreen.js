240066fe47fa921cf3ec4443075aefff7fee7625
Pearlynn <pearlynntoh@gmail.com>
2022-06-13 13:18:24 +0800

    Added category to Activity Screen

--- screens/ActivityScreen.js
+++ screens/ActivityScreen.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Title } from "react-native-paper";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
+
+const category = [
+  { cat: "Sports & Fitness" },
+  { cat: "Health & Wellness" },
+  { cat: "Social" },
+  { cat: "Hobbies & Interests" },
+  { cat: "Volunteer" },
+];
 
 export default function ActivityScreen() {
   return (
     <View style={styles.container}>
-      <Text>Activity</Text>
+      <FlatList
+        data={category}
+        renderItem={({ item }) => {
+          return <Title>{item.cat}</Title>;
+        }}
+        keyExtractor={(item) => item.cat}
+      />
     </View>
   );
 }
@@ -15,7 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    padding: 20,
   },
 });
