db7541a45f9f7574ca33f01f374bb1c8e3a40498
kurifurando <98199527+kurifurando@users.noreply.github.com>
2022-06-14 00:12:56 +0800

    Overwritten from tutorial

--- App.js
+++ App.js
@@ -1,75 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import HomeScreen from "./screens/HomeScreen";
-import ActivityScreen from "./screens/ActivityScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import { FontAwesome } from "@expo/vector-icons";
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+/*
+import LoginScreen from './src/screens'
+import HomeScreen from './src/screens'
+import RegistrationScreen from './src/screens'
+*/
 
 const Stack = createStackNavigator();
 
-const Tab = createBottomTabNavigator();
-
-function Main() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === "Home") {
-            iconName = "home";
-          } else if (route.name === "Activity") {
-            iconName = "list";
-          } else if (route.name === "Profile") {
-            iconName = focused ? "user" : "user-o";
-          }
-
-          return <FontAwesome name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "darkblue",
-        tabBarInactiveTintColor: "gray",
-      })}
-    >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Activity" component={ActivityScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
-  );
-}
-
 export default function App() {
-  let isLoggedIn = true; // Change isLoggedIn to true to view home, activity, profile screens OR isLoggedIn to false to view login, register screens
+
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {isLoggedIn ? (
-          <Stack.Screen
-            name="Main"
-            component={Main}
-            options={{ headerShown: false }}
-          />
+        { user ? (
+          <Stack.Screen name="Home">
+            {props => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
         ) : (
           <>
-            <Stack.Screen name="Log In" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" children={() => (<LoginScreen/>)} />
+            <Stack.Screen name="Registration" children={() => (<RegistrationScreen/>)} />
           </>
         )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
